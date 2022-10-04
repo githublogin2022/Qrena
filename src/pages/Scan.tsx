87a2b454f072bff8dd4b52f808cac2f7a52987c2
youssef-elshazly <youssef.elshazly@vodafone.com>
2022-10-04 12:33:53 +0200

    feat: Add camera function with permission to scan screen

--- src/pages/Scan.tsx
+++ src/pages/Scan.tsx
@@ -1,20 +1,59 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, ActivityIndicator, Alert } from 'react-native';
+import { Camera, useCameraDevices } from 'react-native-vision-camera';
+import { useScanBarcodes, BarcodeFormat } from 'vision-camera-code-scanner';
+import { useIsFocused } from '@react-navigation/native';
+
+import { permission } from '../modules/common/services';
 
 const Scan: () => JSX.Element = () => {
+  const devices = useCameraDevices();
+  const device = devices.back;
+  const [hasPermission, setHasPermission] = useState(false);
+  const [isScanned, setIsScanned] = useState(false);
+  const [frameProcessor, barcodes] = useScanBarcodes([BarcodeFormat.QR_CODE]);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setHasPermission(await permission.checkPermission(permission.permissionTypes.camera));
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (barcodes && barcodes.length > 0 && isScanned === false) {
+      setIsScanned(true);
+      barcodes.forEach(async (scannedBarcode: any) => {
+        if (scannedBarcode.rawValue !== '') {
+          Alert.alert('QrCode Value', scannedBarcode.rawValue, [
+            {
+              text: 'Cancel',
+              onPress: () => setIsScanned(false),
+              style: 'cancel',
+            },
+          ]);
+        }
+      });
+    }
+  }, [barcodes]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (device == null || !hasPermission) {
+    return <ActivityIndicator />;
+  }
+
   return (
-    <View style={styles.Container}>
-      <Text> welcome from Scan </Text>
-    </View>
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={isScanned ? false : isFocused ? true : false}
+      frameProcessor={frameProcessor}
+      frameProcessorFps='auto'
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  Container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default Scan;
